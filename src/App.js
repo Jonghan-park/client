6ba884c4dda7d29aa23548edadcf334dfbc80807
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-22 20:34:28 -0600

    Update components and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCoctail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCoctail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
