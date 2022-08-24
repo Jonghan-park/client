c56b983dbcf4597394168b7cb935cebc9eb61570
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-24 09:37:44 -0600

    Update spinner

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useGlobalContext } from "../context";
+import Loading from "./Loading";
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
   return <div>CocktailList</div>;
 };
 
